d372e803e32c98f013dbbe626288756c95448a21
Landon Patmore <landon.patmore@gmail.com>
2019-02-06 11:29:06 -0500

    Updates
    - updated loggly messages to now echo instead
    - added error handling for new networking error handling

--- utils/DataPuller.go
+++ utils/DataPuller.go
@@ -2,9 +2,9 @@ package utils
 
 import (
 	"dataPullerWorker/types"
-	"fmt"
 	"github.com/landonp1203/goUtils/loggly"
 	"goUtils/networking"
+	"log"
 )
 
 var stationsList, fileReadError = ReadStationsList()
@@ -15,15 +15,16 @@ func RetrieveStationData() {
 		loggly.Error("There was an error reading the stationsList.txt file.")
 	} else {
 		for _, station := range stationsList {
-			var jsonData = networking.Get("https://tidesandcurrents.noaa.gov/api/datagetter?date=today&station=" + station + "&product=water_temperature&units=english&time_zone=gmt&application=tidal_station_project&format=json")
+			var jsonData, err = networking.Get("https://tidesandcurrents.noaa.gov/api/datagetter?date=today&station=" + station + "&product=water_temperature&units=english&time_zone=gmt&application=tidal_station_project&format=json")
 
-			var waterTemperatureStation = types.WaterTemperatureStation{}
-			DecodeJson(jsonData, &waterTemperatureStation)
+			if err != nil {
+				log.Println(err)
+			} else {
+				var waterTemperatureStation = types.WaterTemperatureStation{}
+				DecodeJson(jsonData, &waterTemperatureStation)
 
-			// Just print to console the actual API response
-			fmt.Printf("%+v\n\n", waterTemperatureStation)
-
-			loggly.Info(waterTemperatureStation.LogStruct())
+				loggly.InfoEcho(waterTemperatureStation.LogStruct())
+			}
 		}
 	}
 }
